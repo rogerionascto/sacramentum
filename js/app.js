fd6a662aa4b6e5b53b4c71ae76d89e814319ac73
ROGERIO FERNANDO DO NASCIMENTO <rogerio.nascto@gmail.com>
2017-06-26 12:24:02 -0300

    alteração para ajustar o menu

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ function criarNomeCompleto(primeiroNome, sobreNome){
 
     return nomecompleto;
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
